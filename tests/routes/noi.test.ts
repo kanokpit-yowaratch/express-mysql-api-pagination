17e53d8d9f663872f486e95261258350cf4354e0
Kanokpit Yowaratch <kanokpit.y@gmail.com>
2024-02-24 22:05:15 +0700

    Add unit test

---
+++ tests/routes/noi.test.ts
@@ -0,0 +1,17 @@
+import * as routes from '../../src/routes'
+
+describe('app', () => {
+    it('test route noi', () => {
+        const request = { query: { id: 1, name: 'noi' } };
+        const result = routes.noi(request, null);
+        expect(result).toEqual(`ID: ${request.query.id}, Name: ${request.query.name}`);
+
+        // const req = {
+        //     query: {}
+        // };
+        // req.query = "?id=1&name=noi";
+
+        // Success but => ID: undefined, Name: undefined
+        // const result = routes.noi(req, null);
+    });
+});
